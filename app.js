4fb1a86e2784f4c7e5fc8d9b8c63601b30882a71
Alison Souza <alisonranier@gmail.com>
2023-07-05 11:32:10 -0300

    last_time updates

--- app.js
+++ app.js
@@ -16,6 +16,37 @@ require("./utils/app/manager/index");
 app.get('/', (req,res)=> {
   res.sendFile(path.join(__dirname,'index.html'));
 })
+
+app.get('/routes/', (req, res) => {
+  res.sendFile(path.join(__dirname,'routes.html'));
+});
+
+app.get('/routes/all', (req, res) => {
+  res.json(router.stack);
+});
+
+
+router.get('/posts/', (req,res) => {
+  axios.get(`${process.env.PUB_MANAGER_URL}/pubs/user/${req.params.userId}`)
+    .then(response => {
+      res.json(response);
+    })
+})
+
+router.get('/posts/:userId/', (req, res) => {
+  // To do when connect with proxy
+  // axios.get(`${process.env.PUB_MANAGER_URL}pubs/user/${req.params.userId}`)
+  //   .then(response => {
+  //     res.json(response);
+  //   })
+
+  const posts = JSON.parse(require("./models/mockup/posts.json"));
+  
+  const mapped = posts.map(item => item.author.id === req.params.userId)
+
+  res.json(mapped);
+})
+
 router.get('/twitter/:id/update', (req,res) => {
   axios.get(`${process.env.TWITTER_PROXY_URL}/posts/${req.params.id}`)
     .then(response => {
@@ -23,11 +54,20 @@ router.get('/twitter/:id/update', (req,res) => {
     })
 })
 
-router.get('/user/:id', (req, res) => {
-  // axios.get(`${process.env.USER_MANAGER_URL}/`)
-  res.statusCode = 404
-  res.send();
+router.get('/user/:userId', (req, res) => {
+  axios.get(`${process.env.USER_MANAGER_URL}/users/${req.params.userId}`)
+    .then(response => {
+      if (response.statusCode == 200) {
+        res.json(response);
+      }
+    })
+    .catch(err => {
+      res.statusCode = 500;
+      res.json(err);
+    })
 })
+
+app.use('/', router);
 
 app.listen(app.get("port"), () => {
   console.log("Express server listening on port " + app.get("port"));
